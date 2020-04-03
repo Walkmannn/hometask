56274a75675ce5a8e43e912cc594a1fa1c41aea2
Ivan Voznyakovsky <ivan.voznyakovsky@gmail.com>
2020-04-03 14:38:05 +0300

    refactor to functions

--- homework_5/js/script.js
+++ homework_5/js/script.js
@@ -1,24 +1,60 @@
-let menuItem = document.createElement('li'), //создаю новый элемент в меню
-    menu = document.querySelector('ul'),//Получаю элементы в меню
-    title = document.getElementById('title'), // Получаю название элемента в массиве
-    menuItems = document.querySelectorAll('.menu .menu-item'), //Получаю массив элементов в меню
-    menuItemsSecond = menuItems[2], //Получаю третий элемент меню
-    menuItemsThird = menuItems[1], //Получаю второй элемент меню
-    adv = document.querySelector('div.adv'), //Получаю рекламный блок
-    ans = prompt("Ваше отношение к технике Apple"), //Получаю ответ от пользователя
-    promptClass = document.getElementById('prompt'); //Получаю элемент, куда вставить ответ пользователя
+function removeAds() {
+    document.querySelector('.adv').remove();
+}
 
-    console.log(menu);
+function setTitle(title) {
+    document.getElementById('title').innerText = title;
+}
 
-adv.remove(); //удаляю рекламу
-promptClass.innerHTML = ans; //вставляю ответ пользователя в нужные блок
-menu.appendChild(menuItem); //добавляю новые элемент в меню
-menuItem.classList.add('menu-item'); //добавляю в новый элемент класс
-menuItem.innerHTML = 'Пятый пункт'; //Добавляю в новый элемент название
-menu.insertBefore(menuItemsSecond, menuItemsThird); //переставляю местами пункты меню
-title.innerHTML = 'Мы продаем только подлинную технику Apple'; //меняю надпись
-document.body.style.backgroundImage = "url('./img/apple_true.jpg')"; //меняю фон
+function changeBackgroundImage(imageUrl) {
+    document.body.style.backgroundImage = `url("${imageUrl}")`;
+}
 
+function askUser() {
+    const userAnswer = window.prompt("Ваше отношение к технике Apple");
 
+    document.getElementById('prompt').innerText = userAnswer;
+}
 
+function addMenuItem(itemText) {
+    const newMenuItemElement = document.createElement('li');
 
+    newMenuItemElement.innerText = itemText;
+    newMenuItemElement.classList.add('menu-item');
+
+    document.querySelector('ul.menu').appendChild(newMenuItemElement);
+}
+
+/**
+ * @param {Number} currentIndex Zero-based current element index
+ * @param {Number} newIndex Zero-based new element index
+ */
+function setMenuItemIndex(currentIndex, newIndex) {
+    if (currentIndex === newIndex) {
+        return console.warn('Какого доебался? Оно и так на том месте');
+    }
+
+    const menuItems = document.querySelectorAll('ul.menu > li');
+    const itemToMove = menuItems[currentIndex];
+    const itemToMoveBeforeOrAfter = menuItems[newIndex];
+
+    if (newIndex < currentIndex ) {
+        itemToMove.parentNode.insertBefore(itemToMove, itemToMoveBeforeOrAfter);
+    } else if (newIndex > currentIndex) {
+        itemToMove.parentNode.insertBefore(itemToMove, itemToMoveBeforeOrAfter.nextSibling);
+    }
+}
+
+// html готов, но еще не нарисован, можно редачить
+window.addEventListener('DOMContentLoaded', () => {
+    removeAds();
+    setTitle('Мы продаем только подлинную технику Apple');
+    changeBackgroundImage('./img/apple_true.jpg');
+    addMenuItem('Пятый пункт');
+
+    // меняем местами 3й и 2й
+    setMenuItemIndex(2, 1);
+});
+
+// вся страница готова и нарисована
+window.addEventListener('load', askUser);
